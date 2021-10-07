6652fd58baf88a4a19ae0eebfcf8d1e929d9bc02
sheikhmasud999 <wizesonat@gmail.com>
2021-10-07 12:31:00 +0600

    test commit

---
+++ android/app/src/main/kotlin/com/example/salauddin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.salauddin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const MyApp());
+
+}
+String btnText="Button 1";
+String btnText2="Button 2";
+String btnText3="Button 3";
+
+Color btnClr1= Colors.indigo;
+Color btnClr2= Colors.black87;
+Color btnClr3= Colors.teal;
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("My Frist App"),
+      ),
+      body: Container(
+        child: Align(
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            //crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              RaisedButton(
+                  child: Text(btnText,style: TextStyle(color: Colors.white),),
+                  color: btnClr1,
+                  onPressed: (){
+                    setState(() {
+                      btnClr1=Colors.orangeAccent;
+                      btnText="Button Pressed";
+                    });
+
+                  }),
+             Container(
+               height: 300,
+               width: 400,
+               child: Image.asset("assets/images/image2.jpg"),
+             )
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
